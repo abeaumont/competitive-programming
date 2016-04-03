18c2e6e2008663c0bd30c5cd98a01fe07c759faa
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2016-04-03 19:59:34 +0200

    [lambda-calculi-march-2016] Improved version of l6
    The problem included a merge action but Core.Std.Heap doesn't provide a
    merge function (even if it's a pairing heap and it has the merge
    function implemented, since is needed por the pop function). So I
    implemented my own pairing heap with merge support to improve the
    solution performance

--- hackerrank/lambda-calculi-march-2016/l6.ml
+++ hackerrank/lambda-calculi-march-2016/l6.ml
@@ -1,42 +1,51 @@
-open Core.Std
+(* Pairing heap with merge support *)
+type heap = Empty | Node of int * heap list
+exception EmptyHeap
 
-let find_strongest a merges i =
-  let f top x =
-    match Heap.top a.(x) with
-      None -> top
-    | Some(top') -> max top top' in
-  List.fold_left merges.(i) ~f ~init:Int.min_value
-and strongest_died a merges i =
-  let rec loop index value = function
-    | [] -> Heap.pop_exn a.(index)
-    | x::xs -> match Heap.top a.(x) with
-        None -> loop index value xs
-      | Some(v) ->
-        if v > value
-        then loop x v xs
-        else loop index value xs in
-  loop Int.min_value 0 merges.(i)
-and recruit a i c = Heap.add a.(i) c
-and merge merges i j =
-  merges.(i) <- List.append merges.(i) merges.(j);;
+let merge heap1 heap2 = match (heap1, heap2) with
+    (Empty, x) | (x, Empty) -> x
+  | (Node(i1, s1), Node(i2, s2)) ->
+    if i1 > i2
+    then Node(i1, heap2 :: s1)
+    else Node(i2, heap1 :: s2)
+
+let top = function
+    Empty -> raise EmptyHeap
+  | Node(i, _) -> i
+
+let push heap value =
+  merge (Node(value, [])) heap
+
+let pop = function
+    Empty -> raise EmptyHeap
+  | Node(i, s) ->
+    let rec merge_pairs = function
+        [] -> Empty
+      | [x] -> x
+      | x::y::xs -> merge (merge x y) (merge_pairs xs) in
+    merge_pairs s
+
+let find_strongest a i = top a.(i)
+and strongest_died a i = a.(i) <- pop a.(i)
+and recruit a i c = a.(i) <- push a.(i) c
+and merge_heaps a i j = a.(i) <- merge a.(i) a.(j)
 
 let () =
   let (n, q) = Scanf.sscanf (read_line ()) "%d %d" (fun n q -> (n, q)) in
-  let a = Array.init n (fun _ -> (Heap.create ~cmp:(Fn.flip compare) ())) in
-  let merges = Array.init n (fun i -> [i]) in
+  let a = Array.init n (fun _ -> Empty) in
   for _ = 1 to q do
     let line = read_line () in
     match line.[0] with
       '1' ->
       let (_, i) = Scanf.sscanf line "%d %d" (fun k i -> (k, i)) in
-      print_endline (string_of_int (find_strongest a merges (i - 1)))
+      print_endline (string_of_int (find_strongest a (i - 1)))
     | '2' ->
       let (_, i) = Scanf.sscanf line "%d %d" (fun k i -> (k, i)) in
-      ignore (strongest_died a merges (i - 1))
+      ignore (strongest_died a (i - 1))
     | '3' ->
       let (_, i, c) = Scanf.sscanf line "%d %d %d" (fun k i c -> (k, i, c)) in
-      recruit a (i - 1) c
-    | '4' ->
+      ignore (recruit a (i - 1) c)
+    | _ ->
       let (_, i, j) = Scanf.sscanf line "%d %d %d" (fun k i j -> (k, i, j)) in
-      merge merges (i - 1) (j - 1)
+      merge_heaps a (i - 1) (j - 1)
   done
