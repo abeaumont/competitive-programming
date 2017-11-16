f0d5fcab03e3b305132627b95ae552276dae1d35
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2017-11-16 18:32:36 +0100

    [11679] Sub-prime

---
+++ 11679.cc
@@ -0,0 +1,28 @@
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=2726
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vl;
+
+int main() {
+	while (true) {
+		int b, n;
+		cin >> b >> n;
+		if (!n && !b) break;
+		vl r(b);
+		for (int i = 0; i < b; i++) cin >> r[i];
+		for (int i = 0; i < n; i++) {
+			int d, c, v;
+			cin >> d >> c >> v;
+			d--;
+			c--;
+			r[d] -= v;
+			r[c] += v;
+		}
+		bool ok = true;
+		for (int i = 0; i < b; i++) if (r[i] < 0) ok = false;
+		cout << (ok ? "S" : "N") << endl;
+	}
+}
