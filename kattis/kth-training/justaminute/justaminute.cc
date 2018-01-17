dca19850ab5699408ff9c1d43a676cef3cc63000
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-01-17 17:34:01 +0100

    [kattis] Add C++ solution for Just a Minute problem.

---
+++ kattis/kth-training/justaminute/justaminute.cc
@@ -0,0 +1,18 @@
+// https://open.kattis.com/problems/justaminute
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n, m = 0, s = 0;
+  cin >> n;
+  while (n--) {
+    int a, b;
+    cin >> a >> b;
+    m += a;
+    s += b;
+  }
+  if (s <= m * 60) cout << "measurement error\n";
+  else cout << fixed << setprecision(7) << double(s) / double(m * 60) << endl;
+}
