efb7482af6f41efd183cbe435f155ececf1c57d0
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-01-17 16:49:11 +0100

    [kattis] Add Python 2 solution for Bus Numbers problem.

---
+++ kattis/kth-training/busnumbers/busnumbers.py
@@ -0,0 +1,19 @@
+# https://open.kattis.com/problems/busnumbers
+raw_input()
+xs = map(int, raw_input().split())
+xs.sort()
+i = 0
+ss = []
+while i < len(xs):
+    j = 0
+    while i + j < len(xs):
+        if xs[i + j] == xs[i] + j:
+            j += 1
+        else:
+            break
+    if j > 2:
+        ss.append('{}-{}'.format(xs[i], xs[i+j-1]))
+    else:
+        ss.append(' '.join(map(str, xs[i:i+j])))
+    i += j
+print ' '.join(ss)
