510bcac8104038fb3655953c0b2f6af3ff341bfc
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-01-18 20:24:44 +0100

    [kattis] Add C++ solution for Apaxiaaaaaaaaaaaans! problem.

---
+++ kattis/university-of-chicago-masters-program-in-computer-science-placement-exam-2014-15/apaxiaaans/apaxiaaans.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	string s;
+	cin >> s;
+	string r;
+	unsigned char ch = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (ch && s[i] == ch) continue;
+		ch = s[i];
+		r.push_back(ch);
+	}
+	cout << r << endl;
+}
