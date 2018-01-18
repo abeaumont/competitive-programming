8a68042842a697fb57cfd1621d1478884f2a8b09
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-01-18 13:52:05 +0100

    [kattis] Add Python 2 solution for Oddities problem.

---
+++ kattis/2013-acm-icpc-north-american-qualifier/oddities/oddities.py
@@ -0,0 +1,7 @@
+# https://open.kattis.com/problems/oddities
+for i in range(input()):
+    a = input()
+    if a % 2 == 0:
+        print '{} is even'.format(a)
+    else:
+        print '{} is odd'.format(a)
---
+++ kattis/2013-acm-icpc-north-american-qualifier/oddities/sample.ans
@@ -0,0 +1,3 @@
+10 is even
+9 is odd
+-5 is odd
