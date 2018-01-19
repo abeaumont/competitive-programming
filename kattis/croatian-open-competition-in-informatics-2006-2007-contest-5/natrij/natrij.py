4a4351315a870d645bd02e0a0fd74b5d54b460ba
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-01-19 21:43:39 +0100

    [kattis] Add Python 2 solution for Natrij problem.

---
+++ kattis/croatian-open-competition-in-informatics-2006-2007-contest-5/natrij/natrij.py
@@ -0,0 +1,12 @@
+# https://open.kattis.com/problems/natrij
+xs = [raw_input() for _ in range(2)]
+xs = map(lambda x: map(int, x.split(':')), xs)
+[a, b] = map(lambda x: x[0] * 3600 + x[1] * 60 + x[2], xs)
+if a >= b:
+    b += 24 * 60 * 60
+d = b - a
+h = d / (60 * 60)
+ms = d - (h * 60 * 60)
+m = ms / 60
+s = ms % 60
+print '{}:{}:{}'.format(str(h).zfill(2), str(m).zfill(2), str(s).zfill(2))
