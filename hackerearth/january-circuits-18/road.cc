e55e1df9c7f3b00671884b3cc55b1b8db5829a94
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-01-30 13:20:01 +0100

    [hackerearth - jan-circuits-18] Update Road solution to use DP

--- hackerearth/january-circuits-18/road.cc
+++ hackerearth/january-circuits-18/road.cc
@@ -5,28 +5,25 @@
 using namespace std;
 
 typedef vector<int> vi;
-typedef vector<vi> vvi;
 
-vvi cache;
+int best = 0;
 
-int s(const vi &v, int i, int k) {
-  if (cache[i][k] != -1) return cache[i][k];
-  int c = 0;
-  for (int j = i + 1; j < v.size(); j++) {
-    int k2 = k - abs(v[i] - v[j]);
-    if (k2 < 0) continue;
-    c = max(c, 1 + s(v, j, k2));
+void s(const vi &v, int i, int c, int k) {
+  if (k < 0) return;
+  if (i == v.size() - 1) {
+    best = max(best, c);
+    return;
   }
-  cache[i][k] = c;
-  return c;
+  for (int j = i + 1; j < v.size(); j++) {
+    s(v, j, c + 1, k - abs(v[i] - v[j]));
+  }
 }
 
 int main() {
   int n, k;
   cin >> n >> k;
   vi v(n);
-  cache = vvi(n);
-  for (int i = 0; i < n; i++) cache[i] = vi(k + 1, -1);
   for (int i = 0; i < n; i++) cin >> v[i];
-  cout << 1 + s(v, 0, k) << endl;
+  s(v, 0, 1, k);
+  cout << best << endl;
 }
