6f04df734fda94feee5dac07fbfd2b5f4c371679
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-01-30 13:24:30 +0100

    [hackerearth - jan-circuits-18] Reduce search space in Road solution

--- hackerearth/january-circuits-18/road.cc
+++ hackerearth/january-circuits-18/road.cc
@@ -1,25 +1,32 @@
 // https://www.hackerearth.com/challenge/competitive/january-circuits-18/algorithm/road-1-63e2e618/
 #include <iostream>
+#include <set>
 #include <vector>
 
 using namespace std;
 
+typedef set<int> si;
 typedef vector<bool> vb;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 
 vvi cache;
+si sets[51];
 
 int s(const vi &v, int i, int k) {
   if (cache[i][k] != -1) return cache[i][k];
   int c = 0;
-  vb u(k + 1, false);
-  for (int j = i + 1; j < v.size(); j++) {
-    int k2 = k - abs(v[i] - v[j]);
-    if (k2 < 0) continue;
-    if (!u[k2]) {
-      u[k2] = true;
-      c = max(c, 1 + s(v, j, k2));
+  for (int j = 0; j <= k; j++) {
+    int a = v[i];
+    int b = a + j;
+    if (b <= 50) {
+      auto it = sets[b].upper_bound(i);
+      if (it != sets[b].end()) c = max(c, 1 + s(v, *it, k - j));
+    }
+    b = a - j;
+    if (b >= 0) {
+      auto it = sets[b].upper_bound(i);
+      if (it != sets[b].end()) c = max(c, 1 + s(v, *it, k - j));
     }
   }
   cache[i][k] = c;
@@ -32,6 +39,9 @@ int main() {
   vi v(n);
   cache = vvi(n);
   for (int i = 0; i < n; i++) cache[i] = vi(k + 1, -1);
-  for (int i = 0; i < n; i++) cin >> v[i];
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+    sets[v[i]].insert(i);
+  }
   cout << 1 + s(v, 0, k) << endl;
 }
