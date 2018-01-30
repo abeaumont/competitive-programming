3458925460b3d7a1e8d55a4a6232c5a166290f79
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-01-30 13:21:19 +0100

    [hackerearth - jan-circuits-18] Improve Road's DP solution

--- hackerearth/january-circuits-18/road.cc
+++ hackerearth/january-circuits-18/road.cc
@@ -4,26 +4,34 @@
 
 using namespace std;
 
+typedef vector<bool> vb;
 typedef vector<int> vi;
+typedef vector<vi> vvi;
 
-int best = 0;
+vvi cache;
 
-void s(const vi &v, int i, int c, int k) {
-  if (k < 0) return;
-  if (i == v.size() - 1) {
-    best = max(best, c);
-    return;
-  }
+int s(const vi &v, int i, int k) {
+  if (cache[i][k] != -1) return cache[i][k];
+  int c = 0;
+  vb u(k + 1, false);
   for (int j = i + 1; j < v.size(); j++) {
-    s(v, j, c + 1, k - abs(v[i] - v[j]));
+    int k2 = k - abs(v[i] - v[j]);
+    if (k2 < 0) continue;
+    if (!u[k2]) {
+      u[k2] = true;
+      c = max(c, 1 + s(v, j, k2));
+    }
   }
+  cache[i][k] = c;
+  return c;
 }
 
 int main() {
   int n, k;
   cin >> n >> k;
   vi v(n);
+  cache = vvi(n);
+  for (int i = 0; i < n; i++) cache[i] = vi(k + 1, -1);
   for (int i = 0; i < n; i++) cin >> v[i];
-  s(v, 0, 1, k);
-  cout << best << endl;
+  cout << 1 + s(v, 0, k) << endl;
 }
