4242f1dc0ba5527dfa75b328f66fb2165b31e04e
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-01-30 13:28:24 +0100

    [hackerearth - jan-circuits-18] Use an array instead of set in Road.
    This grants constant instead of logaritmic time for next iteration.

--- hackerearth/january-circuits-18/road.cc
+++ hackerearth/january-circuits-18/road.cc
@@ -11,7 +11,7 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
 
 vvi cache;
-si sets[51];
+vvi sets;
 
 int s(const vi &v, int i, int k) {
   if (cache[i][k] != -1) return cache[i][k];
@@ -20,13 +20,13 @@ int s(const vi &v, int i, int k) {
     int a = v[i];
     int b = a + j;
     if (b <= 50) {
-      auto it = sets[b].upper_bound(i);
-      if (it != sets[b].end()) c = max(c, 1 + s(v, *it, k - j));
+      int a = sets[b][i];
+      if (a) c = max(c, 1 + s(v, a, k - j));
     }
     b = a - j;
     if (b >= 0) {
-      auto it = sets[b].upper_bound(i);
-      if (it != sets[b].end()) c = max(c, 1 + s(v, *it, k - j));
+      int a = sets[b][i];
+      if (a) c = max(c, 1 + s(v, a, k - j));
     }
   }
   cache[i][k] = c;
@@ -39,9 +39,15 @@ int main() {
   vi v(n);
   cache = vvi(n);
   for (int i = 0; i < n; i++) cache[i] = vi(k + 1, -1);
-  for (int i = 0; i < n; i++) {
-    cin >> v[i];
-    sets[v[i]].insert(i);
+  for (int i = 0; i < n; i++) cin >> v[i];
+  sets = vvi(51);
+  for (int i = 0; i <= 50; i++) {
+    sets[i] = vi(n);
+    int a = 0;
+    for (int j = n - 1; j >= 0; j--) {
+      sets[i][j] = a;
+      if (v[j] == i) a = j;
+    }
   }
   cout << 1 + s(v, 0, k) << endl;
 }
