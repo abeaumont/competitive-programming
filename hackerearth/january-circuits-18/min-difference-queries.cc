9c3fb8d884c7411fe1116a6a06160c62da5db382
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-01 18:27:25 +0100

    [hackerearth - jan-circuits-18] Improve Min difference queries solution

--- hackerearth/january-circuits-18/min-difference-queries.cc
+++ hackerearth/january-circuits-18/min-difference-queries.cc
@@ -2,37 +2,18 @@
 #include <cmath>
 #include <algorithm>
 #include <iostream>
+#include <set>
+#include <unordered_set>
 #include <unordered_map>
 #include <vector>
 
 using namespace std;
 
+typedef set<int> si;
+typedef unordered_map<int, int> mii;
+typedef unordered_set<int> usi;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-typedef unordered_map<int, int> mii;
-typedef vector<mii> vmii;
-
-int above(const vvi& b, int i, int k) {
-  int x = 0, y = b[i].size() - 1;
-  while (x <= y) {
-    int m = (x + y) / 2;
-    if (b[i][m] == k) return m;
-    if (b[i][m] < k) x = m + 1;
-    else y = m - 1;
-  }
-  return x;
-}
-
-int below(const vvi& b, int i, int k) {
-  int x = 0, y = b[i].size() - 1;
-  while (x <= y) {
-    int m = (x + y) / 2;
-    if (b[i][m] == k) return m;
-    if (b[i][m] < k) x = m + 1;
-    else y = m - 1;
-  }
-  return y;
-}
 
 int main() {
   int n, q;
@@ -48,14 +29,15 @@ int main() {
   }
   vvi b(k);
   for (int i = 0; i < n; i++) b[a[i]].push_back(i);
-  int c = sqrt(n);
-  vmii s(c + 1);
-  for (int i = 0; i <= c; i++) {
-    for (int j = 0; j < k; j++) {
-      int idx1 = above(b, j, i * c);
-      if (idx1 >= b[j].size() || b[j][idx1] >= (i + 1) * c) continue;
-      int idx2 = below(b, j, (i + 1) * c - 1);
-      s[i][j] = idx2 - idx1 + 1;
+  int c = ceil(sqrt(n));
+  vvi s(c);
+  for (int i = 0; i < n; i += c) {
+    usi d;
+    for (int j = i; j < n; j++) {
+      if (d.count(a[j])) continue;
+      s[i / c].push_back(a[j]);
+      d.insert(a[j]);
+      if (s[i / c].size() > c) break;
     }
   }
   int last = 0;
@@ -66,44 +48,32 @@ int main() {
     if (l < 0) l += n;
     r = (v + last) % n;
     if (r < 0) r += n;
-    mii f;
     int x = l / c;
-    int y = r / c;
-    if (x == y) for (int j = l; j <= r; j++) f[a[j]]++;
-    else {
-      if (l % c) for (int j = l; j < (x + 1) * c; j++) f[a[j]]++;
-      else for (auto z : s[x]) f[z.first] += z.second;
-      for (int j = x + 1; j < y; j++) for (auto z : s[j]) f[z.first] += z.second;
-      if (r % c == c - 1) for (auto z : s[y]) f[z.first] += z.second;
-      else for (int j = y * c; j <= r; j++) f[a[j]]++;
+    si d;
+    bool found = false;
+    for (auto y : s[x]) {
+      int idx1 = lower_bound(b[y].begin(), b[y].end(), l) - b[y].begin();
+      int idx2 = upper_bound(b[y].begin(), b[y].end(), r) - b[y].begin();
+      int z = idx2 - idx1;
+      if (!z) continue;
+      if (d.count(z)) {
+        found = true;
+        break;
+      }
+      d.insert(z);
     }
-    if (f.size() == 1) {
+    if (found) {
+      last = 0;
+    } else if (d.size() <= 1) {
       last = -1;
     } else {
-      vi t(r - l + 1, 0);
-      bool found = false;
-      for (auto z : f) {
-        if (t[z.second]) {
-          found = true;
-          break;
-        }
-        t[z.second]++;
+      int M = r - l + 2;
+      int x = -1;
+      for (auto y : d) {
+        if (x != -1) M = min(M, y - x);
+        x = y;
       }
-      if (found) {
-        last = 0;
-      } else {
-        int M = r - l + 2;
-        int j = 0;
-        while (!t[j]) j++;
-        for (int o = j + 1; o <= r - l; o++) {
-          if (t[o]) {
-            M = min(M, o - j);
-            if (M == 1) break;
-            j = o;
-          }
-        }
-        last = M;
-      }
+      last = M;
     }
     cout << last << endl;
   }
