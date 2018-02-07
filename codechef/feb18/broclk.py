5db77c70e792d58cd61cc30ff1d35b62fc0fb182
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-07 22:31:16 +0100

    [codechef - feb18] Add partial Python 2 solution to BROCLK problem.
    It only solves subtask #1

---
+++ codechef/feb18/broclk.py
@@ -0,0 +1,19 @@
+# https://www.codechef.com/FEB18/problems/BROCLK
+mod = 1000000007
+
+def modinv(a):
+    b, x0, x1 = mod, 0, 1
+    while a > 1:
+	q, a, b = a / b, b, a % b
+        x0, x1 = x1 - q * x0, x0
+    return x1
+
+def chevyshev(n, p):
+    a, b = 1, p
+    for _ in xrange(n - 1): b, a = 2 * p * b - a, b
+    return b
+
+for _ in range(input()):
+    l, d, t = map(int, raw_input().split())
+    x = d * modinv(l)
+    print chevyshev(t, x) * l % mod
