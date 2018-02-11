b150d0ad30f1b1be2939e5ed56bc32824ddc945a
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-11 22:48:05 +0100

    [cses] Add C++ solution for Two Knights.

---
+++ cses/two-knights.cc
@@ -0,0 +1,17 @@
+// https://cses.fi/problemset/task/1072/
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	int n;
+	cin >> n;
+	for (ll i = 1; i <= n; i++) {
+		ll a = i * i * (i * i - 1) / 2;
+		ll b = 2 * (i - 2) * (2 * (i - 4) + 6);
+		cout << a - b << endl;
+	}
+	return 0;
+}
