fcbfd26e5967d949324d55364373cf9e9b597d6d
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-12 12:56:18 +0100

    [hackerrank - python] Add solution for The Minion Game problem

---
+++ hackerrank/python/the-minion-game.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/the-minion-game
+x = [0, 0]
+s = raw_input()
+for i, c in enumerate(s):
+    x[c in 'AEIOU'] += len(s) - i
+if x[0] == x[1]: print 'Draw'
+elif x[0] > x[1]: print 'Stuart', x[0]
+else: print 'Kevin', x[1]
