1aaad8365b4788706248c14d15d18d647aa234fb
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-13 13:11:02 +0100

    [cses] Add C++ solution for Apartments problem

---
+++ cses/apartments.cc
@@ -0,0 +1,27 @@
+// https://cses.fi/problemset/task/1084/
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+
+int main() {
+	int n, m, k;
+	cin >> n >> m >> k;
+	vi a(m);
+	for (int i = 0; i < n; i++) cin >> a[i];
+	vi b(m);
+	for (int i = 0; i < m; i++) cin >> b[i];
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	int i = 0;
+	int j = 0;
+	int c = 0;
+	while (i < n && j < m)
+		if (a[i] + k < b[j]) i++;
+		else if (a[i] - k > b[j]) j++;
+		else { i++; j++; c++; }
+	cout << c << endl;
+}
