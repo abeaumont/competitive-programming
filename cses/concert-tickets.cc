3c1cc77358944d8e43676bf8a936e20d017e2ad4
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-13 13:47:24 +0100

    [cses] Add C++ solution for Concert Tickets problem

---
+++ cses/concert-tickets.cc
@@ -0,0 +1,25 @@
+// https://cses.fi/problemset/task/1091/
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n, m, x;
+	cin >> n >> m;
+	multiset<int, greater<int>> h;
+	while (n--) {
+		cin >> x;
+		h.insert(x);
+	}
+	while (m--) {
+		cin >> x;
+		auto it = h.lower_bound(x);
+		if (it == h.end()) cout << "-1\n";
+		else {
+			cout << *it << endl;
+			h.erase(it);
+		}
+	}
+}
