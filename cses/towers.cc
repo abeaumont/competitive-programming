18e221baf619dd992883f4c95c1a0c63c27da73b
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-13 13:55:21 +0100

    [cses] Add C++ solution for Towers problem

---
+++ cses/towers.cc
@@ -0,0 +1,21 @@
+// https://cses.fi/problemset/task/1073/
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+	int n, x;
+	cin >> n;
+	multiset<int> s;
+	while (n--) {
+		cin >> x;
+		auto it = s.lower_bound(x + 1);
+		if (it == s.end()) s.insert(x);
+		else {
+			s.erase(it);
+			s.insert(x);
+		}
+	}
+	cout << s.size() << endl;
+}
