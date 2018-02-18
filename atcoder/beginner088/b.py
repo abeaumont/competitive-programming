21a3282a26e3a695ec39259faf5f720c75246aff
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-18 16:18:11 +0100

    [atcoder - abc088] Add Python 2 solution for Card Game for Two

---
+++ atcoder/beginner088/b.py
@@ -0,0 +1,11 @@
+n = input()
+a = map(int, raw_input().split())
+a.sort(reverse=True)
+s1 = 0
+s2 = 0
+for i, k in enumerate(a):
+    if i % 2 == 0:
+        s1 += k
+    else:
+        s2 += k
+print s1 - s2
