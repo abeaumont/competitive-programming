77604b8d34c399fa263d4a9839794be9df22304f
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-22 19:34:20 +0100

    [kattis] Add Python 2 solution for Secret Message

---
+++ kattis/secretmessage.py
@@ -0,0 +1,21 @@
+# https://open.kattis.com/problems/secretmessage
+import math
+
+def e(s):
+    l = len(s)
+    k = int(math.ceil(math.sqrt(l)))
+    m = k * k
+    s = s + '*' * (m - k)
+    M = []
+    for i in range(k):
+        M.append(s[i*k:(i+1)*k])
+    Mt = [None] * k
+    for i in range(k):
+        Mt[i] = [None] * k
+        for j in range(k):
+            Mt[i][j] = M[k-j-1][i]
+    Mt = map(lambda row: filter(lambda x: x != '*', row), Mt)
+    return ''.join(map(lambda x: ''.join(x), Mt))
+
+for i in range(input()):
+    print e(raw_input())
