4fb07a0cc7a224a956c3b933da3e84d8f1964734
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-23 00:34:38 +0100

    [kattis] Add C++ solution for Quick Estimates

---
+++ kattis/quickestimate.cc
@@ -0,0 +1,14 @@
+// https://open.kattis.com/problems/quickestimate
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		cout << s.size() << endl;
+	}
+}
