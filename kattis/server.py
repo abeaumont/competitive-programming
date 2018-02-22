1051703fc02c34f9f5db81953f6c83e72218d423
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-02-22 19:28:05 +0100

    [kattis] Add Python 2 solution for Server problem

---
+++ kattis/server.py
@@ -0,0 +1,12 @@
+# https://open.kattis.com/problems/server
+def count(xs, t):
+    s = 0
+    for i, x in enumerate(xs):
+        if x + s > t:
+            return i
+        s += x
+    return len(xs)
+
+t = int(raw_input().split()[1])
+xs = map(int, raw_input().split())
+print count(xs, t)
