0f825fc146e3cb050f22c3574eb633d8dd7a7abc
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-11 14:09:24 +0100

    [atcoder - abc090] Add Python 2 solution for Remainder Reminder

---
+++ atcoder/beginner090/d.py
@@ -0,0 +1,7 @@
+# https://abc090.contest.atcoder.jp/tasks/arc091_b
+n, k = map(int, raw_input().split())
+c = 0
+for b in range(k + 1, n + 1):
+    for i in range((n - k) / b + 1):
+        c += min(n + 1, b * (i + 1)) - max((k + b * i), 1)
+print c
