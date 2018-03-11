1736164aeb0d1df88bc6d72ffcf1f811f88d3554
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-11 14:08:57 +0100

    [atcoder - abc090] Add Python 2 solution for Flip,Flip, and Flip......

---
+++ atcoder/beginner090/c.py
@@ -0,0 +1,7 @@
+# https://abc090.contest.atcoder.jp/tasks/arc091_a
+n, m = map(int, raw_input().split())
+if n + m == 2: print 1
+elif n + m == 3: print 0
+elif n == 1: print m - 2
+elif m == 1: print n - 2
+else: print (n - 2) * (m - 2)
