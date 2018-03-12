5a086fe2d0b9fa863553de639f62f6abeefbb0a2
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-12 11:19:36 +0100

    [codechef - march18a] Add a naive Python2 solution for Pishty and Triangles

---
+++ codechef/march18a/pshtrg.py
@@ -0,0 +1,16 @@
+n, q = map(int, raw_input().split())
+a = map(int, raw_input().split())
+for _ in range(q):
+    t, l, r = map(int, raw_input().split())
+    l -= 1
+    if t == 1:
+        a[l] = r
+    else:
+        x = a[l:r]
+        x.sort(reverse=True)
+        z = 0
+        for i in range(2, len(x)):
+            if x[i - 2] < x[i - 1] + x[i]:
+                z = sum(x[i - 2:i + 1])
+                break
+        print z
