b9e0ac3dad8e4681a6f713d9bd493068cebea879
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-12 17:26:15 +0100

    [atcoder - abc081] Add Python2 solution for Not so Diverse

---
+++ atcoder/abc081/c.py
@@ -0,0 +1,16 @@
+# https://abc081.contest.atcoder.jp/tasks/arc086_a
+[n, k] = map(int, raw_input().split())
+xs = map(int, raw_input().split())
+d = dict()
+for x in xs:
+    if x in d:
+        d[x] += 1
+    else:
+        d[x] = 1
+v = d.values()
+v.sort()
+if len(v) <= k:
+    r = []
+else:
+    r = v[:len(v)-k]
+print sum(r)
