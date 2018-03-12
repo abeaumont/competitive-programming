f0070798107b72be5b3d84a5a4f386a6bdeeebaa
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-12 13:26:48 +0100

    [atcoder - abc085] Add input generator and output verifier for Otoshidama

---
+++ atcoder/beginner085/cchk.py
@@ -0,0 +1,8 @@
+[n, y] = map(int, open('c.in').readline().split())
+[a, b, c] = map(int, open('c.out').read().split())
+if a + b + c == n and a * 10000 + b * 5000 + c * 1000 == y:
+    print "OK"
+else:
+    print "ERROR"
+    print open('c.in').read()
+    print open('c.out').read()
---
+++ atcoder/beginner085/cgen.py
@@ -0,0 +1,8 @@
+import random
+
+a = random.randint(0, 10)
+b = random.randint(0, 10)
+c = random.randint(0, 10)
+
+print a + b + c, a * 10000 + b * 5000 + c * 1000
+print a, b, c
