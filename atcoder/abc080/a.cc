e581dbd90c28f69fff45f6b100122958a8060d02
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-13 11:44:10 +0100

    [atcoder - abc080] Add C++ solution for Parking

---
+++ atcoder/abc080/a.cc
@@ -0,0 +1,10 @@
+// https://abc080.contest.atcoder.jp/tasks/abc080_a
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n, a, b;
+	cin >> n >> a >> b;
+	cout << min(a * n, b) << endl;
+}
