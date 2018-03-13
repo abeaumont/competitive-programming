4302e00f2a687daa086620298c7754da6aeafb71
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-13 15:40:10 +0100

    [atcoder - abc074] Add C++ solution for Bichrome Cells

---
+++ atcoder/abc074/a.cc
@@ -0,0 +1,12 @@
+// https://abc074.contest.atcoder.jp/tasks/abc074_a
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	int n, w;
+	cin >> n >> w;
+	cout << n * n - w << endl;
+}
