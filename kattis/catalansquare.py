15bf4784e04362d6da7631657bc90df118bfc632
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-14 18:28:26 +0100

    [kattis] Add Python2 solution for Catalan Square

---
+++ kattis/catalansquare.py
@@ -0,0 +1,9 @@
+# https://open.kattis.com/problems/catalansquare
+def c(n):
+    a = 1
+    b = 1
+    for i in xrange(n + 1, 2 * n + 1): a *= i
+    for i in xrange(1, n + 1): b *= i
+    return a / b / (n + 1)
+
+print c(input() +1)
