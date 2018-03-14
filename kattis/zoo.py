f7bcacec3f04e2fb88006f271b17350c7b5467e5
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-14 22:38:04 +0100

    [kattis] Add Python2 solution for Un-bear-able Zoo

---
+++ kattis/zoo.py
@@ -0,0 +1,12 @@
+i = 1
+while True:
+    n = input()
+    if n == 0: break
+    d = {}
+    for _ in xrange(n):
+        x = raw_input().split()[-1].lower()
+        d[x] = d.get(x, 0) + 1
+    print 'List {}:'.format(i)
+    for k in sorted(d.keys()):
+        print '{} | {}'.format(k, d[k])
+    i += 1
