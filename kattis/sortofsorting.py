30f0b1eefc749677bcff572403bfbeed5a651ea0
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-15 02:41:54 +0100

    [kattis] Add Python2 solution for Sort of Sorting

---
+++ kattis/sortofsorting.py
@@ -0,0 +1,10 @@
+# https://open.kattis.com/problems/sortofsorting
+init = True
+while True:
+    n = input()
+    if n == 0: break
+    s = [raw_input() for _ in range(n)]
+    s.sort(cmp=lambda x, y: cmp(x[:2], y[:2]))
+    if init: init = False
+    else: print ''
+    for x in s: print x
