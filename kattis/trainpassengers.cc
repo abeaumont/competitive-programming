8b50a80b947abd718bcb23145dd238ab25b573d3
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-16 01:08:32 +0100

    [kattis] Add problem link in Train Passengers solution

--- kattis/trainpassengers.cc
+++ kattis/trainpassengers.cc
@@ -1,20 +1,21 @@
+// https://open.kattis.com/problems/trainpassengers
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    int k, n, s = 0;
-    cin >> k >> n;
-    bool ok = true;
-    while (n-- && ok) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        if (a > s) ok = false;
-        s -= a;
-        if (s + b > k) ok = false;
-        s += b;
-        if (c && s < k) ok = false;
-        if (!n && (c || s)) ok = false;
-    }
-    cout << (ok ? "possible\n" : "impossible\n");
+	int k, n, s = 0;
+	cin >> k >> n;
+	bool ok = true;
+	while (n-- && ok) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		if (a > s) ok = false;
+		s -= a;
+		if (s + b > k) ok = false;
+		s += b;
+		if (c && s < k) ok = false;
+		if (!n && (c || s)) ok = false;
+	}
+	cout << (ok ? "possible\n" : "impossible\n");
 }
