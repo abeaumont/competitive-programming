2794a78520e19b624eee748e98193be04571d2ef
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-18 00:44:39 +0100

    [kattis] Add Python2 solution for Wizard of Odds

---
+++ kattis/wizardofodds.py
@@ -0,0 +1,8 @@
+# https://open.kattis.com/problems/wizardofodds
+[n, k] = map(int, raw_input().split())
+i = 0
+while n > 1:
+    i += 1
+    n = (n + 1) / 2
+if i <= k: print 'Your wish is granted!'
+else: print 'You will become a flying monkey!'
