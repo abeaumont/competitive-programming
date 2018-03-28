7d098f8c7eba96c178a0f9166c6f351eef1010ad
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-28 20:30:13 +0200

    [atcoder - abc059] Add Python3 solution for Comparison

---
+++ atcoder/abc059/b.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# https://abc059.contest.atcoder.jp/tasks/abc059_b
+a = int(input())
+b = int(input())
+if a == b: print('EQUAL')
+elif a < b: print('LESS')
+else: print('GREATER')
