a5cd1edab33a3343c14ebd79c18f3e521415f50b
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-30 13:52:14 +0200

    [atcoder - abc062] Add Python3 solution for Chocolate Bar

---
+++ atcoder/abc062/c.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# https://beta.atcoder.jp/contests/abc062/tasks/arc074_a
+def s(a1, a2, a3): return max([a1, a2, a3]) - min([a1, a2, a3])
+
+h, w = map(int, input().split())
+h1 = h // 3
+h2 = (h - h1) // 2
+h3 = h - h1 - h2
+t = s(h1 * w, h2 * w, h3 * w)
+w1 = w // 3
+w2 = (w - w1) // 2
+w3 = w - w1 - w2
+t = min(t, s(h * w1, h * w2, h * w3))
+w1 = w // 3
+w2 = w - w1
+h1 = h // 2
+h2 = h - h1
+t = min(t, s(h * w1, h1 * w2, h2 * w2))
+w1 = (w // 3) + 1
+w2 = w - w1
+t = min(t, s(h * w1, h1 * w2, h2 * w2))
+h1 = h // 3
+h2 = h - h1
+w1 = w // 2
+w2 = w - w1
+t = min(t, s(h1 * w, h2 * w1, h2 * w2))
+h1 = h // 3 + 1
+h2 = h - h1
+t = min(t, s(h1 * w, h2 * w1, h2 * w2))
+print(t)
