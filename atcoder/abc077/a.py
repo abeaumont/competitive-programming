ead5dbc7aaf13a98585b40130500910fcb3b84be
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-03-31 22:40:13 +0200

    [atcoder - abc077] Add Python3 solution for Rotation

---
+++ atcoder/abc077/a.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# https://abc077.contest.atcoder.jp/tasks/abc077_a
+s = input()
+t = input()
+if s[0] == t[2] and s[1] == t[1] and s[2] == t[0]: print('YES')
+else: print('NO')
