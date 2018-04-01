dc36090a0ea54483a3de39cff3fee79d5fe60201
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-04-01 10:42:19 +0200

    [hackerrank - ruby] Add Ruby solution for Ruby Control Structures - Case (Bonus Question)

---
+++ hackerrank/ruby/ruby-case.rb
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/ruby-case
+def identify_class(obj)
+  case obj
+  when Hacker
+    puts "It's a Hacker!"
+  when Submission
+    puts "It's a Submission!"
+  when TestCase
+    puts "It's a TestCase!"
+  when Contest
+    puts "It's a Contest!"
+  else
+    puts "It's an unknown model"
+  end
+end
