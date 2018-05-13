05b247918a01a2ad9282dff2f8dc619635f079e1
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-05-13 17:58:42 +0200

    [atcoder] Make de IO scrapper async

--- atcoder/get-samples.py
+++ atcoder/get-samples.py
@@ -1,59 +1,61 @@
 #!/usr/bin/env python3
 # Get input/output samples for a contest
+import asyncio
 import os
 import sys
 
-import requests
-
+import aiohttp
 from bs4 import BeautifulSoup
+
+
+class BadStatusException(Exception):
+    pass
 
 
 class TaskException(Exception):
     pass
 
 
-def get_task(name: str, url: str) -> None:
-    response = requests.get(url)
-    if response.status_code != requests.codes.ok:
-        raise TaskException('Invalid status code {}'.format(response.status_code))
-    soup = BeautifulSoup(response.text, 'html.parser')
-    sections = soup.find_all('section')
-    for section in sections:
-        h3 = section.h3.text
-        if h3.startswith('Sample'):
-            _, io, n = h3.split()
-            if io.lower().startswith('in'):
-                fname = '{}-{}.{}'.format(name, n, 'in')
-            else:
-                fname = '{}-{}.{}'.format(name, n, 'ans')
-            with open(fname, 'w') as f:
-                f.write(section.pre.text.replace('\r\n', '\n'))
+async def process_task(client: aiohttp.ClientSession, name: str, url: str) -> None:
+    print('Getting samples for task {}'.format(name))
+    async with client.get(url) as response:
+        if response.status != 200:
+            raise TaskException(
+                'Invalid status code {}'.format(response.status))
+        soup = BeautifulSoup(await response.text(), 'html.parser')
+        sections = soup.find_all('section')
+        for section in sections:
+            h3 = section.h3.text
+            if h3.startswith('Sample'):
+                _, io, n = h3.split()
+                if io.lower().startswith('in'):
+                    fname = '{}-{}.{}'.format(name, n, 'in')
+                else:
+                    fname = '{}-{}.{}'.format(name, n, 'ans')
+                with open(fname, 'w') as f:
+                    f.write(section.pre.text.replace('\r\n', '\n'))
+    print('Got samples for task {}'.format(name))
 
 
-def main(contest: str) -> int:
-    print('Getting samples for contest {}...'.format(contest))
-    prefix = 'https://' + contest + '.contest.atcoder.jp'
-    url = prefix + '/assignments'
-    response = requests.get(url)  # pyre-ignore[18]
-    if response.status_code != requests.codes.ok:
-        print('Something went wrong on request to {}: {}'.format(
-            url, response.status_code), sys.stderr)
-        return -1
-    soup = BeautifulSoup(response.text, 'html.parser')
-    table = soup.find_all('tbody')[0]
-    for row in table.find_all('tr'):
-        task = row.td.a
-        name = task.text.lower()
-        url = prefix + task.get('href')
-        try:
-            print('Getting samples for task {}'.format(name))
-            get_task(name, url)
-            print('Got samples for task {}'.format(name))
-        except Exception as e:
-            print('Error getting task {} from {}: {}'.format(
-                name, url, e), sys.stderr)
-            return -2
-    return 0
+async def main(loop: asyncio.BaseEventLoop, contest: str) -> None:
+    async with aiohttp.ClientSession(loop=loop) as client:
+        print('Getting samples for contest {}...'.format(contest))
+        prefix = 'https://' + contest + '.contest.atcoder.jp'
+        url = prefix + '/assignments'
+        async with client.get(url) as response:
+            if response.status != 200:
+                raise BadStatusException(
+                    'Bad status for {}: {}'.format(url, response.status))
+            soup = BeautifulSoup(await response.text(), 'html.parser')
+            table = soup.find_all('tbody')[0]
+            tasks = []
+            for row in table.find_all('tr'):
+                task = row.td.a
+                name = task.text.lower()
+                url = prefix + task.get('href')
+                task = asyncio.ensure_future(process_task(client, name, url))
+                tasks.append(task)
+            await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
@@ -61,4 +63,5 @@ if __name__ == '__main__':
         contest = sys.argv[1]
     else:
         contest = os.path.basename(os.path.abspath(os.curdir))
-    sys.exit(main(contest))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop, contest))
