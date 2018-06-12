ca332eefd72a8056ebdff655a5cb0f3d077d1350
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-06-12 09:58:43 +0200

    [cses] Add C++ solution for Tree Diameter

---
+++ cses/tree-diameter.cc
@@ -0,0 +1,54 @@
+// https://cses.fi/problemset/task/1131/
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <unordered_set>
+
+using namespace std;
+
+typedef tuple<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef queue<ii> qii;
+typedef unordered_set<int> si;
+
+int main() {
+	int n;
+	cin >> n;
+	vvi g(n);
+	for (int i = 0; i < n - 1; i++) {
+		int a, b;
+		cin >> a >> b;
+		a--;
+		b--;
+		g[a].push_back(b);
+		g[b].push_back(a);
+	}
+	qii q;
+	si s;
+	q.push({0, 0});
+	s.insert(0);
+	int a, b, k;
+	while (!q.empty()) {
+		tie(a, k) = q.front();
+		q.pop();
+		for (auto z : g[a]) {
+			if (s.count(z)) continue;
+			s.insert(z);
+			q.push({z, k + 1});
+		}
+	}
+	q.push({a, 0});
+	s.clear();
+	while (!q.empty()) {
+		tie(a, k) = q.front();
+		q.pop();
+		for (auto z : g[a]) {
+			if (s.count(z)) continue;
+			s.insert(z);
+			q.push({z, k + 1});
+		}
+	}
+	cout << k << endl;
+}
