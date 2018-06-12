bf0c20fafe018d81b7e5d642ab651271629af14b
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-06-12 23:55:28 +0200

    [cses] Add C++ solution for Exponentiation

---
+++ cses/exponentiation.cc
@@ -0,0 +1,28 @@
+// https://cses.fi/problemset/task/1095/
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+const int M = 1000000007;
+
+int pow(ll a, ll b, int m) {
+	ll r = 1, e = a;
+	while (b) {
+		if (b & 1) r = (r * e) % m;
+		e = (e * e) % m;
+		b >>= 1;
+	}
+	return r;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	while (n--) {
+		int a, b;
+		cin >> a >> b;
+		cout << pow(a, b, M) << endl;
+	}
+}
