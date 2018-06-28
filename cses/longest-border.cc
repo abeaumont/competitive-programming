530a38592040b8f8457d10f35da14cc48a79f663
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-06-28 09:54:28 +0200

    [cses] Add C++ solution for Longest Border

---
+++ cses/longest-border.cc
@@ -0,0 +1,21 @@
+// https://cses.fi/problemset/task/1109/
+#include <iostream>
+
+using namespace std;
+
+int x, y, n, a[1000001];
+
+int main() {
+	string s;
+	cin >> s;
+	n = s.size();
+	int i = 1;
+	for (;i < n; i++) {
+		a[i] = max(0, min(a[i - x], y - i + 1));
+		while (a[i] + i < n && s[a[i]] == s[a[i] + i]) {
+			x = i; y = a[i] + i; a[i]++;
+		}
+		if (i + a[i] == n) break;
+	}
+	cout << s.substr(i, a[i]) << endl;
+}
