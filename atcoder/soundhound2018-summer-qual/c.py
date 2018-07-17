d158a7b1e627e8d3d31bdd5e499e0343067b7f1a
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-07-17 18:13:32 +0200

    [atcoder - soundhound2018-summer-qual] Add Python3 solution for Ordinary Beauty

---
+++ atcoder/soundhound2018-summer-qual/c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# https://soundhound2018-summer-qual.contest.atcoder.jp/tasks/soundhound2018_summer_qual_c
+n, m, d = map(int, input().split())
+if d == 0: print('{:.10f}'.format((m - 1) / n))
+else:
+    t = n * (n - 1) // 2
+    print('{:.10f}'.format((m - 1) * (n - 1) * (n - d) / (t * n)))
