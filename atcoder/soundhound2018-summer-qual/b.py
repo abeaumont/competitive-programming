ab4127704e36080c152de66e82896c710dfe3653
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-07-17 18:12:10 +0200

    [atcoder - soundhound2018-summer-qual] Add Python3 solution for Acrostic

---
+++ atcoder/soundhound2018-summer-qual/b.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# https://soundhound2018-summer-qual.contest.atcoder.jp/tasks/soundhound2018_summer_qual_b
+s = input()
+n = int(input())
+print(''.join([s[i] for i in range(0, len(s), n)]))
