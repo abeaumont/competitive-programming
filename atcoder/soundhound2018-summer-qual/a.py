9a3969b22cf27352606bddaa1168ee367c40566a
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-07-17 18:11:03 +0200

    [atcoder - soundhound2018-summer-qual] Add Python3 solution for F

---
+++ atcoder/soundhound2018-summer-qual/a.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# https://soundhound2018-summer-qual.contest.atcoder.jp/tasks/soundhound2018_summer_qual_a
+a, b = map(int, input().split())
+if a * b == 15: print('*')
+elif a + b == 15: print('+')
+else: print('x')
