aeeb9cd7d8f7b899f8928865f57d40287d85ef15
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-08-28 19:44:26 +0200

    [codechef - aug18a] Add complete C++ solution for Coordinate Compression

--- codechef/aug18a/kcompres.cc
+++ codechef/aug18a/kcompres.cc
@@ -1,65 +1,88 @@
 // https://www.codechef.com/AUG18A/problems/KCOMPRES
 #include <algorithm>
 #include <iostream>
-#include <functional>
-#include <numeric>
+#include <tuple>
 #include <vector>
 
 using namespace std;
 
 typedef long long ll;
-
-typedef vector<bool> vb;
+typedef tuple<ll, ll> ii;
 typedef vector<ll> vi;
-typedef vector<vi> vvi;
+typedef vector<ii> vii;
+
+vi tree;
+ll n, nn;
+
+void update(ll k, ll v) {
+  ll i = k + nn;
+  tree[i] = v;
+  for (i /= 2; i > 0 ; i /= 2) tree[i] = max(tree[2*i], tree[2*i+1]);
+}
+
+ll qmax(ll a, ll b) {
+  a += nn; b += nn;
+  ll m = 0;
+  while (a <= b) {
+    if (a % 2) m = max(m, tree[a++]);
+    if (!(b % 2)) m = max(m, tree[b--]);
+    a /= 2; b /= 2;
+  }
+  return m;
+}
 
 int main() {
-  ll t, n, s;
+  int t;
   cin >> t;
   while (t--) {
+    ll s;
     cin >> n >> s;
-    vi a(n);
-    for (ll i = 0; i < n; i++) cin >> a[i];
+    vii a(n);
+    for (int i = 0; i < n; i++) {
+      ll x;
+      cin >> x;
+      a[i] = {x, i};
+    }
     if (s < n) {
       cout << 0 << endl;
       continue;
+    } else if (n == 1) {
+      cout << 2 << endl;
+      continue;
     }
+    sort(a.begin(), a.end());
     auto sum = [&](ll k) {
-      vvi g(n);
-      for (ll i = 0; i < n; i++)
-        for (ll j = i + 1; j <= min(n-1, i+k); j++) {
-          if (a[i] <= a[j]) g[i].push_back(j);
-          if (a[i] >= a[j]) g[j].push_back(i);
+      nn = 1;
+      while (n > nn) nn *= 2;
+      tree = vi(2*nn);
+      ll i = 0, j = -1, l = -1, v = -1;
+      vi ps;
+      while (i < n) {
+        ll x, y;
+        tie(x, y) = a[i];
+        if (v == -1) {
+          v = x;
+          j = max(0LL, y - k);
+          l = min(n - 1, y + k);
+          ps.push_back(y);
+          i++;
+        } else if (v == x && y <= l) {
+          l = min(n - 1, y + k);
+          ps.push_back(y);
+          i++;
         }
-
-      vi p;
-      vb v(n);
-      function<void(ll)> dfs = [&](ll x) {
-        for (ll y:g[x]) {
-          if (v[y]) continue;
-          v[y] = true;
-          dfs(y);
-        }
-        p.push_back(x);
-      };
-      for (ll i = 0; i < n; i++)
-        if (!v[i]) {
-          v[i] = true;
-          dfs(i);
-        }
-      reverse(p.begin(), p.end());
-
-      vi b(n, 1);
-      for (ll i = 0; i < p.size(); i++) {
-        ll x = p[i];
-        for (auto y:g[x]) {
-          if (b[x] + (a[x] != a[y]) > b[y]) {
-            b[y] = b[x] + (a[x] != a[y]);
-            p.push_back(y);
-          }
+        else {
+          v = qmax(j, l) + 1;
+          for (auto z:ps) update(z, v);
+          v = -1;
+          ps.resize(0);
         }
       }
-      return accumulate(b.begin(), b.end(), 0);
+      v = qmax(j, l) + 1;
+      for (auto z:ps) update(z, v);
+      ll s = 0;
+      for (ll i = nn; i < 2*nn; i++) s += tree[i];
+      return s;
     };
     ll k = 0;
     for (ll b = n/2; b >= 1; b /= 2)
