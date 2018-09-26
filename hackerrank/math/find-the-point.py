b7895abbe9340d63e4d70bc3ec3bb3d0b57ab25c
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-09-26 21:12:40 +0200

    [hackerrank - math] Add Python3 solution for Find the Point

---
+++ hackerrank/math/find-the-point.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/find-point
+for _ in range(int(input())):
+    px,py,qx,qy = map(int, input().split())
+    rx,ry=2*qx-px,2*qy-py
+    print(rx,ry)
