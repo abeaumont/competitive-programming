d718b408f62d67d8d29ed0bd0d0ba6e2ae2f2e81
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-09-29 20:05:52 +0200

    [atcoder - arc103] Add C++ solution for D - Robot Arms

---
+++ atcoder/arc103/d.cc
@@ -0,0 +1,64 @@
+// https://arc103.contest.atcoder.jp/tasks/arc103_b
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+const int N = 1000;
+ll x[N], y[N];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> x[i] >> y[i];
+  ll p = (llabs(x[0])+llabs(y[0]))%2, m = 0;
+  bool ok = true;
+  for (int i = 0; i < n; i++) {
+    if (p != (llabs(x[i])+llabs(y[i]))%2) {
+      ok = false;
+      break;
+    }
+    m = max(m, llabs(x[i]) + llabs(y[i]));
+  }
+  if (ok) {
+    vector<ll> v;
+    v.push_back(1);
+    if (p==0) v.push_back(1);
+    while (v.back()*2 < m+p) v.push_back(v.back()*2);
+    cout << v.size() << endl;
+    for (int i = 0; i < v.size(); i++) {
+      cout << v[i];
+      if (i < v.size() - 1) cout << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+      ll l = x[i], r = y[i];
+      string s;
+      for (int j = v.size() - 1; j >= 0; j--) {
+        if (llabs(l) > llabs(r)) {
+          if (l < 0) {
+            l += v[j];
+            s.push_back('L');
+          } else {
+            l -= v[j];
+            s.push_back('R');
+          }
+        } else {
+          if (r < 0) {
+            r += v[j];
+            s.push_back('D');
+          } else {
+            r -= v[j];
+            s.push_back('U');
+          }
+        }
+      }
+      assert(!l); assert(!r);
+      reverse(s.begin(), s.end());
+      cout << s << endl;
+    }
+  } else cout << "-1\n";
+}
