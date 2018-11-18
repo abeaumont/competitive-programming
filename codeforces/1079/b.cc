b981d890ef3d2a440ebf82fbab08dc7a91a9365f
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-11-18 20:25:36 +0100

    [codeforces - 1079] Add C++ solution for B. Personalized Cup

---
+++ codeforces/1079/b.cc
@@ -0,0 +1,20 @@
+// https://codeforces.com/contest/1079/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+  string s;
+  cin>>s;
+  int n=s.size();
+  int r=(n-1)/20+1;
+  int c=(n-1)/r+1;
+  int k=r*c-n;
+  cout<<r<<" "<<c<<endl;
+  for(int l=0,i=0;i<r;i++) {
+    for(int j=0;j<c;j++)
+      if(j==c-1&&i<k)cout<<'*';
+      else cout<<s[l++];
+    cout<<endl;
+  }
+}
