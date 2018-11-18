d42ce49aa7c537eb35fb156d51f74efc9e2ba017
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-11-18 20:21:15 +0100

    [codeforces - 4] Add C++ solution for A. Watermelon

---
+++ codeforces/4/a.cc
@@ -0,0 +1,10 @@
+// https://codeforces.com/contest/4/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+  int w;
+  cin>>w;
+  cout<<((w%2||w==2)?"NO\n":"YES\n");
+}
