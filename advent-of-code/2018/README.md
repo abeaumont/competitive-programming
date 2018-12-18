1187987e2d335dafb91e3a5361f5d03dd451d7cf
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2018-12-18 03:30:22 +0100

    [advent-of-code - 2018] Add JavaScript solution for Day 17

--- advent-of-code/2018/README.md
+++ advent-of-code/2018/README.md
@@ -18,3 +18,4 @@ A new solution in a different language every day!
 - [Day 14](./day14) ([Factor](https://factorcode.org/)): [Part I](./day14/part1.factor), [Part II](./day14/part2.factor)
 - [Day 15](./day15) ([Go](https://golang.org/)): [Part I](./day15/part1.go), [Part II](./day15/part2.go)
 - [Day 16](./day16) ([ooc](https://ooc-lang.org/)): [Part I](./day16/part1.ooc), [Part II](./day16/part2.ooc)
+- [Day 17](./day17) ([JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript/)): [Part I](./day17/part1.js), [Part II](./day17/part2.js)
---
+++ advent-of-code/2018/day17/part2.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+const rl = require('readline').createInterface({input: process.stdin})
+
+let c = []
+rl.on('line', (l) => {
+    let [a, b] = l.split(', ')
+    if (a.indexOf('y') == -1) {
+        [a, b] = [b, a]
+    }
+    c.push([a, b].map((e) => {
+        const v = e.split('=')[1]
+        const fs = v.indexOf('.') == -1 ? [v, v] : v.split('..')
+        return [parseInt(fs[0]), parseInt(fs[1])]
+    }))
+})
+
+rl.on('close', () => {
+    const ymax = Math.max(...c.map((x) => x[0][1])) + 1
+    const ymin = Math.min(...c.map((x) => x[0][0]))
+    const xmax = Math.max(...c.map((x) => x[1][1])) + 2
+
+    let m = new Array(ymax).fill().map(_ => Array(xmax).fill('.'))
+    m[0][500] = '+'
+    for (const [y, x] of c)
+        for (let i = y[0]; i <= y[1]; i++)
+            for (let j = x[0]; j <= x[1]; j++)
+                m[i][j] = '#'
+
+    const fill = (y, x, d) => {
+        if (m[y][x] == '#') return false
+        m[y][x] = '|'
+        if (m[y + 1][x] == '|') return true
+        if (m[y + 1][x] == '.' && (down(y + 1, x))) return true
+        if (fill(y, x + d, d)) return true
+        m[y][x] = '~'
+        return false
+    }
+
+    const replace = (y, x, d) => {
+        for (let i = x; m[y][i] == '~'; i += d) m[y][i] = '|'
+    }
+
+    const rowfill = (y, x) => {
+        m[y][x] = '~'
+        const p1 = fill(y, x + 1, 1)
+        const p2 = fill(y, x - 1, -1)
+        if (p1) replace(y, x, -1)
+        if (p2) replace(y, x, 1)
+        return p1 || p2
+    }
+
+    const down = (y, x) => {
+        m[y][x] = '|'
+        if (y == ymax - 1) return true
+        if (m[y + 1][x] == '#') return rowfill(y, x)
+        if (down(y + 1, x)) return true
+        m[y][x] = '~'
+        return rowfill(y, x)
+    }
+
+    down(1, 500)
+    let s = 0
+    for (let i = ymin; i < ymax; i++)
+        s += m[i].reduce((a, x) => a + (x == '~' ? 1 : 0), 0)
+    console.log(s)
+})
