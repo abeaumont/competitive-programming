cae91177304f8780c020d14392e1805551f8654e
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-01-11 16:49:46 +0100

    [codeforces - acmsguru] Add C++ solution for 100 - A+B.

---
+++ codeforces/acmsguru/100.cc
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemsets/acmsguru/problem/99999/100
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a,b;
+  cin>>a>>b;
+  cout<<a+b<<"\n";
+}
