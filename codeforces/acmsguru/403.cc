882452b3fdb4ad13a6134e8e784ffbb24221933c
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-01-11 17:10:35 +0100

    [codeforces - acmsguru] Add C++ solution for 403 - Scientific Problem.

---
+++ codeforces/acmsguru/403.cc
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemsets/acmsguru/problem/99999/403
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int x;
+  cin>>x;
+  cout<<x*2+1<<"\n";
+}
