7f4972e8cd98db9b118ee10dde0437e6c56972d9
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-01-11 18:51:15 +0100

    [codeforces - acmsguru] Add C++ solution for 103 - Div 3.

---
+++ codeforces/acmsguru/105.cc
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemsets/acmsguru/problem/99999/105
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin>>n;
+  n--;
+  cout<<n/3*2+n%3<<"\n";
+}
