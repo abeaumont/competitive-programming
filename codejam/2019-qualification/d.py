00b6a7f4bb40d5121fd8b3b6c66142ea8190f2d2
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-04-08 11:10:21 +0200

    [codejam - 2019-qualification] Add Python3 solution for D. Dat Bae.
    For both F=5 and F=10.

--- codejam/2019-qualification/d.py
+++ codejam/2019-qualification/d.py
@@ -3,18 +3,38 @@
 t=int(input())
 for _ in range(t):
     n,b,f=map(int,input().split())
-    r=[None]*10
-    for i in range(f):
-        s=('1'*(2**(f-i-1))+'0'*(2**(f-i-1)))*(2**i)
-        s=s[-n:]
+    r=[None]*4
+    z=[None]*(n-b)
+    ok=True
+    for i in range(5):
+        a=16//(2**i)
+        s=('0'*a+'1'*a)*(1024//(2*a))
+        s=s[:n]
         print(s)
-        r[i]=input()
-        if int(r[i])==-1:break
-    if r[-1] is None: break
+        q=input()
+        if int(q)==-1:
+            ok=False
+            break
+        if i:r[i-1]=q
+        else:
+            j,k=0,0
+            while j<len(q) and k<n:
+                if q[j]==s[k]:
+                    z[j]=(k//16)*16
+                    j+=1
+                k+=1
+    for i in range(f-5):
+        if not ok:break
+        s='0'*n
+        print(s)
+        if int(input())==-1:
+            ok=False
+            break
+    if not ok:break
     x=[True]*n
     for i in range(n-b):
-        x[int(''.join([r[j][i] for j in range(f)]),2)]=False
-    x=sorted([n-i-1 for i,k in enumerate(x) if k])
+        x[z[i]+int(''.join([r[j][i] for j in range(4)]),2)]=False
+    x=sorted([i for i,k in enumerate(x) if k])
     print(' '.join(map(str,x)))
     if int(input())==-1:
         break
