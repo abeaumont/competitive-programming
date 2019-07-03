62545b53dea517f74c9356482d652a0874746406
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-07-03 09:14:17 +0200

    [cses] Add C++ solution for Range Sum Queries I.

---
+++ cses/range-sum-queries-i.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, q, l, r;
+  cin >> n >> q;
+  vector<long long> a(n + 1);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i + 1];
+    a[i + 1] += a[i];
+  }
+  for (int i = 0; i < q; i++) {
+    cin >> l >> r;
+    cout << a[r] - a[l - 1] << "\n";
+  }
+}
