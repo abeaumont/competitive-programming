27d9b91a62764c710ae579d4c5ee85efc8e786dd
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-08-06 00:17:30 +0200

    [cses] Improve Book Shop solution, use O(n) memory instead.

--- cses/book-shop.cc
+++ cses/book-shop.cc
@@ -5,17 +5,16 @@ using namespace std;
 
 int h[1001];
 int s[1001];
-int a[100002][1001];
+int a[100002];
 
 int main() {
-	int n, x;
-	cin >> n >> x;
-	for (int i = 0; i < n; i++) cin >> h[i];
-	for (int i = 0; i < n; i++) cin >> s[i];
-	for (int i = 1; i <= x; i++)
-		for (int j = 0; j < n; j++) {
-			if (h[j] <= i) a[i][j] = max(a[i][j - 1], a[i - h[j]][j - 1] + s[j]);
-			else a[i][j] = a[i][j - 1];
-		}
-	cout << a[x][n - 1] << endl;
+  int n, x;
+  cin >> n >> x;
+  for (int i = 0; i < n; i++) cin >> h[i];
+  for (int i = 0; i < n; i++) cin >> s[i];
+  for (int i = 0; i < n; i++)
+    for (int j = x; j >= 1; j--)
+      if (h[i] <= j)
+        a[j] = max(a[j], a[j - h[i]] + s[i]);
+  cout << a[x] << endl;
 }
