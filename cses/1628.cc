5187ac5b487d6b20a30d55523f1f215ffff42ed7
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-08-19 23:14:40 +0200

    [cses] Add C++ solution for 1628 - Meet in the Middle.

---
+++ cses/1628.cc
@@ -0,0 +1,29 @@
+// https://cses.fi/problemset/task/1628
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<ll>;
+using mii = map<ll, int>;
+
+ll x, s;
+
+void f(mii& b, vi &a, int i, ll k) {
+  if (k > x) return;
+  if (i == a.size()) b[k]++;
+  else f(b, a, i + 1, k), f(b, a, i + 1, k + a[i]);
+}
+
+int main() {
+  int n, p, q;
+  cin >> n >> x;
+  p = n / 2;
+  q = n - p;
+  vi a(p), b(q);
+  for (int i = 0; i < p; i++) cin >> a[i];
+  for (int i = 0; i < q; i++) cin >> b[i];
+  mii c, d;
+  f(c, a, 0, 0), f(d, b, 0, 0);
+  for (auto y : c) s += y.second * d[x - y.first];
+  cout << s << "\n";
+}
