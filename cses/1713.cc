9b70a812c255a257cbcd43b37041c13327aa9599
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-10-07 00:31:38 +0200

    [cses] Simplify solution for 1713 - Counting Divisors.

--- cses/1713.cc
+++ cses/1713.cc
@@ -2,42 +2,19 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
+using vi = vector<int>;
 
-const ll N = 1000000;
+int N = 1000000;
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie();
-  bitset<N + 1> p;
-  p.set();
-  p[0] = p[1] = 0;
-  for (ll i = 4; i <= N; i += 2) p[i] = 0;
-  for (ll i = 3; i <= N; i += 2)
-    if (p[i])
-      for (ll j = i * i; j <= N; j += 2 * i)
-        p[j] = 0;
-  vector<int> a;
-  for (int i = 0; i <= N; i++)
-    if (p[i])
-      a.push_back(i);
-  int n, x, s, c;
+  vi d(N + 1);
+  for (int i = 1; i <= N; i++)
+    for (int j = i; j <= N; j += i)
+      d[j]++;
+  int n, x;
   cin >> n;
-  for (int i = 0; i < n; i++) {
-    cin >> x;
-    s = 1;
-    for (int y : a) {
-      c = 1;
-      while (!(x % y)) {
-        x /= y;
-        c++;
-      }
-      s *= c;
-      if (y * y >= x) {
-        s *= 1 + (x > 1);
-        break;
-      }
-    }
-    cout << s << "\n";
-  }
+  for (int i = 0; i < n; i++)
+    cin >> x, cout << d[x] << "\n";
 }
