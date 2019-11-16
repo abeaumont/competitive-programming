ee51d7cbfa09f1c9e4a911dfdab478ad971b1911
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-11-16 23:54:15 +0100

    [codeforces - 1061A] Add C++ solution for Coins

---
+++ codeforces/1061/a.cc
@@ -0,0 +1,9 @@
+// https://codeforces.com/contest/1061/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+int main(){
+  ll n,s;
+  cin>>n>>s;
+  cout<<(s-1)/n+1<<endl;
+}
