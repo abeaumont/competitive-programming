6812f08cce1ecb60483fd452eaa1b7f871bf65d6
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-11-17 18:10:36 +0100

    [codeforces - 1186A] Add C++ solution for Vus the Cossack and a Contest

---
+++ codeforces/1186/a.cc
@@ -0,0 +1,13 @@
+// https://codeforces.com/contest/1186/problem/A
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  int n, m, k;
+  cin >> n >> m >> k;
+  cout << (n <= m && n <= k ? "Yes\n" : "No\n");;
+}
