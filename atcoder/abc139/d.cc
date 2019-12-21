3dbaa45d6bfde6804f4490a548ee3690b962ffa9
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-12-22 00:42:50 +0100

    [atcoder - abc139] Add C++ solution for D - ModSum

---
+++ atcoder/abc139/d.cc
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/abc139/tasks/abc139_d
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<ll>;
+
+int main() {
+  ios::sync_with_stdio(0), cin.tie(0);
+  ll n;
+  cin >> n;
+  cout << n * (n - 1) / 2 << "\n";
+}
