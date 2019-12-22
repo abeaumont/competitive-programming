55d6b31faf4911c8370869e7360105514b55dfdb
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2019-12-22 09:50:15 +0100

    [atcoder - nikkei2019-2-qual] Add C++ solution for A - Sum of Two Integers

---
+++ atcoder/nikkei2019-2-qual/a.cc
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/nikkei2019-2-qual/tasks/nikkei2019_2_qual_a
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<ll>;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  cout << (n - 1) / 2 << "\n";
+}
