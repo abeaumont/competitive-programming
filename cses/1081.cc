a6d407049dfc54b09d5f1b328e097cd00001dbc9
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2020-01-26 10:59:28 +0100

    [cses] Fix 1081 - Common Divisors

--- cses/1081.cc
+++ cses/1081.cc
@@ -1,12 +1,12 @@
 // https://cses.fi/problemset/task/1081/
 #include <iostream>
 #include <vector>
-#include <unordered_set>
+#include <set>
 
 using namespace std;
 
 typedef vector<int> vi;
-typedef unordered_set<int> si;
+typedef set<int, greater<int>> si;
 
 const int X = 1000000;
 int a[X+1];
@@ -23,6 +23,8 @@ void divisors(vi& f, si& s, int i, int x) {
 }
 
 int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
   for (int i = 2; i <= X; i++) {
     if (a[i]) continue;
     for (int j = 2*i; j <= X; j += i)
@@ -33,6 +35,10 @@ int main() {
   while (n--) {
     int x; cin >> x;
     if (x < m) continue;
+    if (d[x]) {
+      m = max(m, x);
+      continue;
+    }
     vi f;
     while (a[x]) {
       f.push_back(a[x]);
@@ -42,8 +48,12 @@ int main() {
     si s;
     divisors(f, s, 0, 1);
     for (auto c:s) {
-      if (d[c]) m = max(m, c);
-      else d[c] = true;
+      if (c <= m) break;
+      if (d[c]) {
+        m = max(m, c);
+        break;
+      }
+      d[c] = true;
     }
   }
   cout << m << endl;
