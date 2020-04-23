92330e26f4a12bf6b8dee0a5476acb43b6dc1b99
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-04-23 19:25:43 +0530

    Update ginorts.py

--- hackerrank/python/ginorts.py
+++ hackerrank/python/ginorts.py
@@ -1,21 +1,3 @@
 #!/usr/bin/env python2
 # https://www.hackerrank.com/challenges/ginorts
-from __future__ import print_function
-import string
-
-def compare(a, b):
-    s = string.lowercase
-    if a in s and b in s: return cmp(a, b)
-    if a in s: return -1
-    if b in s: return 1
-    s = string.uppercase
-    if a in s and b in s: return cmp(a, b)
-    if a in s: return -1
-    if b in s: return 1
-    s = '13579'
-    if a in s and b in s: return cmp(a, b)
-    if a in s: return -1
-    if b in s: return 1
-    return cmp(a, b)
-
-print(*sorted(raw_input(), cmp=compare), sep='')
+print(*sorted(input(), key=lambda c: (-ord(c) >> 5, c in '02468', c)), sep='')
