16da381362f0f19e9d683b7ffddb45cf5d522706
Prateek Jena <pjena891@gmail.com>
2020-07-23 13:31:08 +0530

    A648 Matrix Game

---
+++ codeforces/0648/A648.c
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int t,m,n;
+
+main(){
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d %d", &n, &m);
+        int arr[n][m];
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                scanf("%d", &arr[i][j]);
+
+
+    }
+    return 0;
+}
