111902dff75e79e65213c95055ffb0bb15b76e94
div5252 <54735797+div5252@users.noreply.github.com>
2020-10-01 10:58:52 +0530

    Update 1099.cc

--- cses/1099.cc
+++ cses/1099.cc
@@ -1,4 +1,3 @@
-// https://cses.fi/problemset/task/1099/
 #include <iostream>
 #include <vector>
 
@@ -12,8 +11,7 @@ int main() {
     cin >> n;
     vector<int> a(n);
     for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = n - 2; i > 0; i--) a[i] += a[i+1];
-    for (int i = 1; i < n; i++) s ^= a[i];
+    for (int i = 1; i < n; i+=2) s ^= a[i];
     cout << (s ? "first\n" : "second\n");
   }
 }
