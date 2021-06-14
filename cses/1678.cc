5517cd069749b5f25f1caedeab40974a72cae0fe
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2021-06-14 20:54:24 +0200

    [cses] Fix solution for 1678 - Round Trip II.

--- cses/1678.cc
+++ cses/1678.cc
@@ -2,28 +2,25 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using si = set<int>;
 
-vvi g;
-vi r, s;
-si t;
+vector<int> s;
+vector<vector<int>> g;
+vector<int> ans;
 
 bool dfs(int u) {
   s[u] = 1;
-  t.insert(u);
-  for (int v : g[u]) {
-    if (t.count(v)) {
-      r.push_back(v);
+  for (int v : g[u])
+    if (!s[v]) {
+      if (dfs(v)) {
+        if (ans[0] != ans.back()) ans.push_back(u);
+        return true;
+      }
+    } else if (s[v] == 1) {
+      ans.push_back(v);
+      ans.push_back(u);
       return true;
     }
-    if (!s[v]) {
-      r.push_back(v);
-      if (dfs(v)) return true;
-      r.pop_back();
-    }
-  }
+  s[u] = 2;
   return false;
 }
 
@@ -31,28 +28,20 @@ int main() {
   cin.tie(0), ios::sync_with_stdio(0);
   int n, m, u, v;
   cin >> n >> m;
-  g = vvi(n);
+  g = vector<vector<int>>(n);
   for (int i = 0; i < m; i++) {
-    cin >> u >> v, u--, v--;
+    cin >> u >> v;
+    u--, v--;
     g[u].push_back(v);
   }
-  s = vi(n);
-  for (int i = 0; i < n; i++) {
-    if (!s[i]) {
-      t = si();
-      r = vi(1, i);
+  s = vector<int>(n);
+  for (int i = 0; i < n; i++)
+    if (!s[i])
       if (dfs(i)) {
-        reverse(r.begin(), r.end());
-        while (r.back() != r.front()) r.pop_back();
-        if (r.size() <= 2) continue;
-        reverse(r.begin(), r.end());
-        cout << r.size() << '\n';
-        for (int j = 0; j < r.size(); j++)
-          cout << r[j]+1 << " \n"[j == r.size() - 1];
+        cout << ans.size() << '\n';
+        for (int i = 0; i < ans.size(); i++)
+          cout << ans[ans.size() - i - 1] + 1 << " \n"[i == ans.size() - 1];
         return 0;
       }
-    }
-  }
   cout << "IMPOSSIBLE\n";
 }
-
