7119b418752592f233d7e87aa0730f53ea80b121
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2023-04-30 18:56:57 +0200

    [cses] Fix solution for 1190 - Subarray Sum Queries

--- cses/1190.cc
+++ cses/1190.cc
@@ -1,47 +1,38 @@
 // https://cses.fi/problemset/task/1190/
-#include <iostream>
-#include <tuple>
+#include <bits/stdc++.h>
 
 using namespace std;
-
-typedef long long ll;
-typedef tuple<ll,ll,ll,ll> iiii;
-
-const int N = 500000;
-iiii t[4*N];
-int nn,n,m,k;
-ll x,a,a1,a2,b,b1,b2,c,c1,c2,m1,m2, mm;
-
-void update(int i, ll x) {
-  i += nn;
-  t[i] = {x, x, x, x};
-  while (i >= 2) {
-    i /= 2;
-    tie(a1,b1,c1,m1) = t[i*2];
-    tie(a2,b2,c2,m2) = t[i*2+1];
-    b = b1+b2;
-    a = max(a1,max(b1+a2,b));
-    c = max(c2,max(c1+b2,b));
-    mm = max(m1,m2);
-    mm = max(mm, a); mm = max(mm, b); mm = max(mm, c);
-    mm = max(mm, c1+a2);
-    t[i] = {a,b,c,mm};
-  }
-}
+using ll = long long;
+const int N = (1 << 19);
+tuple<ll, ll, ll, ll> t[N];
 
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
-  for (nn = 1; nn < n; nn*=2);
-  for (int i = 0; i < n; i++) {
-    cin >> x;
-    update(i, x);
+  cin.tie(0), ios::sync_with_stdio(0);
+  ll n, q, k = 1, p, x, l1, m1, r1, l2, m2, r2, mx1, mx2;
+  cin >> n >> q;
+  while (k < n) k *= 2;
+  for (int i = 0; i < n; i++)
+    cin >> x, t[k + i] = {max(0LL, x), x, max(0LL, x), max(0LL, x)};
+  for (int i = k - 1; i; i--) {
+    tie(l1, m1, r1, mx1) = t[2 * i];
+    tie(l2, m2, r2, mx2) = t[2 * i + 1];
+    ll l = max(l1, m1 + l2), m = m1 + m2, r = max(m2 + r1, r2),
+       mx = max(r1 + l2, max(mx1, mx2));
+    mx = max(max(l, m), max(r, mx));
+    t[i] = {l, m, r, mx};
   }
-  for (int i = 0; i < m; i++) {
-    cin >> k >> x;
-    update(k-1, x);
-    tie(a,b,c,m1) = t[1];
-    cout << m1 << "\n";
+  while (q--) {
+    cin >> p >> x;
+    int i = k + p - 1;
+    t[i] = {max(0LL, x), x, max(0LL, x), max(0LL, x)};
+    for (i /= 2; i; i /= 2) {
+      tie(l1, m1, r1, mx1) = t[2 * i];
+      tie(l2, m2, r2, mx2) = t[2 * i + 1];
+      ll l = max(l1, m1 + l2), m = m1 + m2, r = max(m2 + r1, r2),
+         mx = max(r1 + l2, max(mx1, mx2));
+      mx = max(max(l, m), max(r, mx));
+      t[i] = {l, m, r, mx};
+    }
+    cout << get<3>(t[1]) << '\n';
   }
 }
