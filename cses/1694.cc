5da8465a8e703016f2108ea10e10d23e5bd1cdb5
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2023-04-30 18:49:22 +0200

    [cses] Fix solution for 1694 - Download Speed

--- cses/1694.cc
+++ cses/1694.cc
@@ -3,56 +3,38 @@
 
 using namespace std;
 using ll = long long;
-using qi = queue<int>;
-
 const int N = 500;
+int n;
+ll g[N][N], c, f;
+vector<bool> s;
 
-int n, l[N + 1], p[N + 1];
-ll f, g[N + 1][N + 1];
-
-bool dfs(int u, ll c) {
-  if (u == n - 1) {
-    f += c;
-    while (u) {
-      g[p[u]][u] -= c;
-      g[u][p[u]] += c;
-      u = p[u];
-    }
-    return 1;
+ll dfs(int i, ll cap) {
+  s[i] = 1;
+  if (i == n - 1) {
+    f += cap;
+    return cap;
   }
-  for (int v = 0; v < n; v++)
-    if (l[v] == l[u] + 1 && g[u][v] > 0) {
-      p[v] = u;
-      if (dfs(v, min(g[u][v], c))) return 1;
-    }
+  for (int j = 0; j < n; j++)
+    if (!s[j] && g[i][j] >= c)
+      if (ll cc = dfs(j, min(cap, g[i][j]))) {
+        g[i][j] -= cc;
+        g[j][i] += cc;
+        return cc;
+      }
   return 0;
 }
 
 int main() {
-  cin.tie(0), ios::sync_with_stdio(0);
-  int m, u, v;
-  ll c;
+  int m, a, b;
   cin >> n >> m;
   for (int i = 0; i < m; i++) {
-    cin >> u >> v >> c;
-    u--, v--;
-    g[u][v] += c;
+    cin >> a >> b >> c;
+    g[a - 1][b - 1] += c;
   }
-  while (1) {
-    fill(l, l + n, -1);
-    qi q;
-    q.push(0);
-    l[0] = 0;
-    while (!q.empty()) {
-      u = q.front();
-      if (u == n - 1) break;
-      q.pop();
-      for (int v = 0; v < n; v++)
-        if (l[v] < 0 && g[u][v] > 0) l[v] = l[u] + 1, q.push(v);
-    }
-    if (q.empty()) break;
-    while (dfs(0, 1e18))
-      ;
+  s = vector<bool>(n);
+  for (c = 1 << 29; c >= 1; c /= 2) {
+    fill(s.begin(), s.end(), 0);
+    while (dfs(0, 1e9)) fill(s.begin(), s.end(), 0);
   }
   cout << f << '\n';
 }
