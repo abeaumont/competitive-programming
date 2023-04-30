facc91fa272d40d7920df64ade8ab27edaf39b8a
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2023-04-30 18:50:23 +0200

    [cses] Fix solution for 1695 - Police Chase

--- cses/1695.cc
+++ cses/1695.cc
@@ -2,65 +2,51 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ii = tuple<int, int>;
-using qi = queue<int>;
-using vii = vector<ii>;
-
+using ll = long long;
 const int N = 500;
+int n;
+ll g[N][N], c, f;
+vector<bool> s;
 
-int n, l[N + 1], p[N + 1];
-int g[N + 1][N + 1];
-
-bool dfs(int u) {
-  if (u == n - 1) {
-    while (u) g[p[u]][u]--, g[u][p[u]]++, u = p[u];
+ll dfs(int i) {
+  s[i] = 1;
+  if (i == n - 1) {
+    f++;
     return 1;
   }
-  for (int v = 0; v < n; v++)
-    if (l[v] == l[u] + 1 && g[u][v]) {
-      p[v] = u;
-      if (dfs(v)) return 1;
-    }
+  for (int j = 0; j < n; j++)
+    if (!s[j] && g[i][j])
+      if (dfs(j)) {
+        g[i][j]--;
+        g[j][i]++;
+        return 1;
+      }
   return 0;
-}
-
-void dfs2(int u) {
-  l[u] = 1;
-  for (int v = 0; v < n; v++)
-    if (!l[v] && g[u][v]) dfs2(v);
 }
 
 int main() {
   cin.tie(0), ios::sync_with_stdio(0);
-  int m, u, v;
+  int m, a, b;
   cin >> n >> m;
   for (int i = 0; i < m; i++) {
-    cin >> u >> v;
-    u--, v--;
-    g[u][v]++, g[v][u]++;
+    cin >> a >> b;
+    a--, b--;
+    g[a][b] = g[b][a] = 1;
   }
-  while (true) {
-    fill(l, l + n, -1);
-    qi q;
-    q.push(0);
-    l[0] = 0;
-    while (!q.empty()) {
-      u = q.front();
-      if (u == n - 1) break;
-      q.pop();
-      for (int v = 0; v < n; v++)
-        if (l[v] < 0 && g[u][v]) l[v] = l[u] + 1, q.push(v);
-    }
-    if (q.empty()) break;
-    while (dfs(0))
-      ;
+  s = vector<bool>(n);
+  while (dfs(0)) fill(s.begin(), s.end(), 0);
+  cout << f << '\n';
+  fill(s.begin(), s.end(), 0);
+  s[0] = 1;
+  queue<int> q;
+  q.push(0);
+  while (!q.empty()) {
+    a = q.front();
+    q.pop();
+    for (int b = 0; b < n; b++)
+      if (!s[b] && g[a][b]) s[b] = 1, q.push(b);
   }
-  fill(l, l + n, 0);
-  dfs2(0);
-  vii e;
   for (int i = 0; i < n; i++)
     for (int j = 0; j < n; j++)
-      if (l[i] && !l[j] && g[j][i]) e.push_back({i, j});
-  cout << e.size() << '\n';
-  for (auto [u, v] : e) cout << u + 1 << ' ' << v + 1 << '\n';
+      if (s[i] && !s[j] && g[j][i]) cout << i + 1 << ' ' << j + 1 << '\n';
 }
