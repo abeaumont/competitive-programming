125d61422972d991f4ea23aa27466038e08d3547
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2023-05-05 19:09:54 +0200

    [kattis] Add C++ solution for Echo Echo Echo

---
+++ kattis/echoechoecho.cc
@@ -0,0 +1,10 @@
+// https://open.kattis.com/problems/echoechoecho
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  cout << s << ' ' << s << ' ' << s << '\n';
+}
